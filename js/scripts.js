e750553e3513b2c6bd2cd57b44712fc56384e2a0
roman-dev <shevchenko.rb@gmail.com>
2018-05-05 14:07:16 +0300

    js correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-document.querySelector('.menu-button').onclick = function() {
-  document.querySelector('.menu-button').classList.toggle('menu-button--active');
-  document.querySelector('.mainmenu').classList.toggle('mainmenu--mobile');
-}
-
-
 var btn = document.querySelector('.subscribe_form__button')
 btn.onmousemove = function (e) {
   var x = e.pageX - btn.offsetLeft
@@ -11,3 +5,10 @@ btn.onmousemove = function (e) {
   btn.style.setProperty('--x', x + 'px')
   btn.style.setProperty('--y', y + 'px')
 }
+
+document.querySelector('.menu-button').onclick = function() {
+  document.querySelector('.menu-button').classList.toggle('menu-button--active');
+  document.querySelector('.mainmenu').classList.toggle('mainmenu--mobile');
+}
+
+
