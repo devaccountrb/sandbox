89b0c8d20fa5c8da0a7810d43c26d3caa64dfa36
roman-dev <shevchenko.rb@gmail.com>
2018-05-05 12:11:23 +0300

    hover button gradient

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ document.querySelector('.menu-button').onclick = function() {
   document.querySelector('.menu-button').classList.toggle('menu-button--active');
   document.querySelector('.mainmenu').classList.toggle('mainmenu--mobile');
 }
+
+
+var btn = document.querySelector('.subscribe_form__button')
+btn.onmousemove = function (e) {
+  var x = e.pageX - btn.offsetLeft
+  var y = e.pageY - btn.offsetTop
+  btn.style.setProperty('--x', x + 'px')
+  btn.style.setProperty('--y', y + 'px')
+}
